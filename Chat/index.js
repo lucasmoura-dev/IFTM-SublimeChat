a5278dd8b7ce1ded8f2add6d5d43578a7df12751
Lucas Moura <lucasmoura.developer@hotmail.com>
2016-10-10 16:40:55 -0300

    Novas alterações

--- Chat/index.js
+++ Chat/index.js
@@ -6,55 +6,90 @@ app.get('/', function(req, res){
 	res.sendFile("index.html", {"root": __dirname});
 });
 
-var online=[];
-io.on('connection', function(socket){
-	var meuNome;
-	//Ao conectar usuario recebe todos os usuarios onlin
-	var msgr={tipo:"userStart", nomes:online};
-	var json=JSON.stringify(msgr);
+
+var usuariosOnline=[];
+
+// Verifico na minha lista de usuários conectados se o nome de usuário é único
+function contemUsuario(nomeUsuario)
+{
+	if(usuariosOnline.length>0)
+	{
+		for(i = 0; i < usuariosOnline.length;i++)
+		{
+			if(usuariosOnline[i] == nomeUsuario)
+				return true;
+		}
+	}
+
+	return false;
+}
+
+// Adiciona o nome do usuário e o seu socket na lista
+function adicionarUsuario(nomeUsuario, socket)
+{
+	usuariosOnline[nomeUsuario] = socket;
+}
+
+// Envia a mensagem contendo todos os usuários online
+function enviarUsuariosOnline()
+{
+	var nomesUsuarios = [];
+	for(nome in usuariosOnline)
+	{
+		nomesUsuarios.push(nome);
+	}
+	var msgr = {tipo:"userStart", nomes:nomesUsuarios};
+	var json = JSON.stringify(msgr);
 	io.emit('chat message', json);
-	console.log('listening on *:3000');
-	//Recebendo mensagens do cliente
+}
+
+console.log("Servidor criado na porta 3000");
+
+
+io.on('connection', function(socket){
+	console.log("Cliente conectado: " + socket.id);
+	var meuNome;
+	enviarUsuariosOnline();
+	// Recebendo mensagens do cliente
+	// Cada socket 
 	socket.on('chat message', function(msg){
-		//Mensagem do cliente é recebida e convertida para array
+		// Mensagem do cliente é recebida e convertida para array
 		dataObj = JSON.parse(msg);
-		//Novo usuario
-		if(dataObj['tipo']=='novo'){
-			var flag=0;
-			if(online.length>0){
-				for(i = 0; i< online.length;i++){
-					if(online[i]==dataObj['nome']){
-						var flag=1
-						break;
-					}
-				}
+
+		if(dataObj['tipo'] == 'novo') // Novo usuario
+		{	
+			if(contemUsuario(dataObj['nome']) == false)
+			{
+				meuNome = dataObj['nome'];
+				adicionarUsuario(meuNome, socket);
+				var msgr = {tipo:"novo", user:meuNome};
 			}
-			//Caso usuario não exista ele entra no chat
-			if(flag==0){
-				meuNome=dataObj['nome'];
-				online.push(dataObj['nome'])
-				var msgr={tipo:"novo", user:dataObj['nome']};
-			//Senão retorna erro
-			}else{
+			else
+			{
 				var msgr={tipo:"erro1", msg:"Usuario jã existe"};
 			}
-		//Mensagem no chat que é repassada para todos
-		}else if(dataObj['tipo']=='all'){
-			var msgr={tipo:"all", msg:dataObj['msg'], from: dataObj['user']};
-		//Mensagem privata que é repassada ao destinatário
-		}else if(dataObj['tipo']=='dm'){
-			var msgr={tipo:"dm", msg:dataObj['msg'], from: dataObj['user'], dest: dataObj['dest']};
 		}
-		//Mensagem é convertida para json e enviada para cliente
-		var json=JSON.stringify(msgr);
+		else if(dataObj['tipo'] == 'all') //Mensagem no chat que é repassada para todos
+		{
+			var msgr = {tipo:"all", msg:dataObj['msg'], from: dataObj['user']};
+		
+		}
+		else if(dataObj['tipo'] == 'dm') //Mensagem privada que é repassada ao destinatário
+		{
+			var msgr = {tipo:"dm", msg:dataObj['msg'], from: dataObj['user'], dest: dataObj['dest']};
+		}
+
+		// Mensagem é convertida para json e enviada para cliente
+		var json = JSON.stringify(msgr);
 		io.emit('chat message', json);
 	});
+
 	//Caso cliente disconectar enviar para todos os usuarios o nome do disconectado
 	socket.on('disconnect', function(){
-		if(online.length>0){
-			for(i=0; i<online.length;i++){
-				if(online[i]==meuNome){
-					online.splice(i,1);
+		if(usuariosOnline.length>0){
+			for(i=0; i< usuariosOnline.length;i++){
+				if(usuariosOnline[i]==meuNome){
+					usuariosOnline.splice(i,1);
 					var msgr={tipo:"offline", user:meuNome};
 					var json=JSON.stringify(msgr);
 					io.emit('chat message', json);
@@ -68,5 +103,4 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
 
